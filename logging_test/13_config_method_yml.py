fbe4e79999dd1574ec4536150a70c7d0f2bf48ec
olly <o.hensby@maxfordham.com>
2021-09-17 17:11:00 +0100

    Added logging config method for yml

---
+++ logging_test/13_config_method_yml.py
@@ -0,0 +1,14 @@
+# Calls file.yml as the config file.
+
+import logging
+import logging.config
+import yaml
+
+with open('logging_test/file.yml', 'r') as f:
+    config = yaml.safe_load(f.read())
+    logging.config.dictConfig(config)
+
+# Get the logger, console_logger, which will print to the console.
+logger = logging.getLogger('console_logger')
+
+logger.info('This is a debug message')
