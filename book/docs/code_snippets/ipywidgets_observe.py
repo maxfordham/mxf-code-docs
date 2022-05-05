9f0cec656761398674438eeba097bf229ee5426f
john <j.gunstone@maxfordham.com>
2022-05-05 17:19:47 +0100

    added code_snippets

---
+++ book/docs/code_snippets/ipywidgets_observe.py
@@ -0,0 +1,40 @@
+
+import ipywidgets as widgets
+import traitlets
+
+class Test(traitlets.HasTraits):
+    value = traitlets.Unicode()
+    def __init__(self, value):
+        self.text = widgets.Text(disabled=True)
+        self.text1 = widgets.Text()
+        self.text.value = value
+        
+class TestControls:
+    def _init_controls(self):
+        self.observe(self._update_change, "value")
+        self.text1.observe(self._update_change1, "value")
+        self.text1.observe(self._update_change1_1, "value")
+        
+    def _update_change(self, onchange):
+        print('_update_change')
+        self.text.value = self.value
+        
+    def _update_change1(self, onchange):
+        print('_update_change1')
+        self.value = self.text1.value + '-ffs'
+        
+    def _update_change1_1(self, onchange):
+        print('_update_change1_1')
+        
+class ParentTest(Test, TestControls):
+    def __init__(self, value):
+        super().__init__(value)
+        self._init_controls()
+        
+    def _ipython_display_(self):
+        display(self.text)
+        display(self.text1)
+        
+if __name__ == "__main__":
+    t = ParentTest('asdf')
+    display(t)
